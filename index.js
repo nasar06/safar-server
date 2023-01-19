2f355287ba66d7aeb28f21cf7e0607680b8d77b8
syed abdul hakim <s.abdulhakim10@gmail.com>
2023-01-19 10:09:25 +0600

    new branch created

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express")
+const cors = require('cors');
 require('dotenv').config()
 
 const port = process.env.PORT || 5000
-const app = express()
+const app = express();
+
+// middle ware
+app.use(cors())
+app.use(express.json())
 
 
 // require route
@@ -10,12 +15,13 @@ const loginRouter = require('./utilities/createjwt')
 const verifyjwtRouter = require('./utilities/verifyjwt')
 
 
-
-
 //router
 app.use("/createjwt", loginRouter)
 app.use("/verifyjwt", verifyjwtRouter)
 
+app.get('/', (req, res) => {
+    res.send('Hello!!!')
+})
 
 app.listen(port, ()=>{
     console.log('server is on')
