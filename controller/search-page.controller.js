fd5ce4e8b7aea3471da2bc3aef09f8bdbf14f59a
nasar06 <nasar5847@gmail.com>
2023-02-02 13:17:09 +0600

    min max price query api create

--- controller/search-page.controller.js
+++ controller/search-page.controller.js
@@ -1,11 +1,27 @@
 const { Destination } = require("../models/destinations.models")
 
 const getDestinationBySearch = async (req, res) => {
-        const result = await Destination.findOne({ city: req.query.location.toUpperCase() })
+        const result = await Destination.findOne({ "location.city": req.query.location.toUpperCase() })
         res.send(result)
 
 
 }
 
+const getDestinationPrice = async (req, res) => {
+       
+        const { min, max, ...others } = req.query;
+        console.log(min, max, others)
+        try {
+                const hotels = await Destination.find({'location.city': others.city,
+                offer_price: { $gt: min || 1, $lt: max || 999 },
+                }).limit(req.query.limit);
+                res.status(200).json(hotels);
+        } catch (err) {
+                res.send(err);
+                console.log(err);
+        }
+}
+
 
 exports.getDestinationBySearch = getDestinationBySearch
+exports.getDestinationPrice = getDestinationPrice
