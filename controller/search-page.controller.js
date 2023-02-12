6a79062ed296cd3c511c24a8a8d924dae2322efe
nasar06 <nasar5847@gmail.com>
2023-02-12 23:29:51 +0600

    search min max price api use regex

--- controller/search-page.controller.js
+++ controller/search-page.controller.js
@@ -4,26 +4,35 @@ const getDestinationBySearch = async (req, res) => {
         // const result = await Destination.find({ "location.city": req.query.location.toUpperCase() })
         // res.send(result)
         const searchTerm = req.query.location.toUpperCase()
-        Destination.find({ "location.city": { $regex: new RegExp(searchTerm, 'i') } }, function(error, results) {
+        Destination.find({ "location.city": { $regex: new RegExp(searchTerm, 'i') } }, function (error, results) {
                 if (error) {
-                  console.error(error);
-                  res.sendStatus(500);
+                        console.error(error);
+                        res.sendStatus(500);
                 } else {
-                  res.send(results);
+                        res.send(results);
                 }
-              });
+        });
 }
 
 const getDestinationPrice = async (req, res) => {
-       
+
         const { min, max, ...others } = req.query;
-        console.log(min, max, others)
         try {
-                const hotels = await Destination.find({'location.city': others.city.toUpperCase(),
-                offer_price: { $gt: min || 1, $lt: max || 999 },
+                // const hotels = await Destination.find({'location.city': others.city.toUpperCase(),
+                // offer_price: { $gt: min || 1, $lt: max || 999 },
+                // });
+                // res.status(200).json(hotels);
+
+                const searchTerm = others.city.toUpperCase()
+                Destination.find({ "location.city": { $regex: new RegExp(searchTerm, 'i') }, offer_price: { $gt: min || 1, $lt: max || 999 }  }, function (error, results) {
+                        if (error) {
+                                console.error(error);
+                                res.sendStatus(500);
+                        } else {
+                                res.send(results);
+                        }
                 });
-                res.status(200).json(hotels);
-                
+
         } catch (err) {
                 res.send(err);
                 console.log(err);
