52228df4502b4bc9f254231f822c9487c8edd36a
nasar06 <nasar5847@gmail.com>
2023-02-23 23:50:18 +0600

    create api for get all order by email

--- controller/orders.controller.js
+++ controller/orders.controller.js
@@ -1,11 +1,29 @@
-const { Order } = require("../models/orders.model")
+const { Orders } = require("../models/orders.model")
 
 
 //post order
 const newOrder = async (req, res)=>{
-  const result = await Order.create(req.body)
+  const result = await Orders.create(req.body)
   res.send(result)
+}
+
+//get all orders by email
+const getallOrderbyEmail = async (req, res)=>{
+  try{
+    const result = await Orders.find({
+      $or: [
+        { customer_email: req.query.email },
+        { status: { $exists: false } }
+      ]
+    })
+    res.send(result)
+  }
+  catch(err){
+    res.send(err)
+    console.log(err)
+  }
 }
 
 
 exports.newOrder = newOrder
+exports.getallOrderbyEmail = getallOrderbyEmail
--- routes/orders.route.js
+++ routes/orders.route.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { newOrder } = require("../controller/orders.controller");
+const { newOrder, getallOrderbyEmail } = require("../controller/orders.controller");
 const router = express.Router();
 
 router.post('/add-order', newOrder)
+router.get('/get-order-byEmail', getallOrderbyEmail)
 
 module.exports = router;
