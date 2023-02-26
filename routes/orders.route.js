ac6615d803a41f2d8a8a8cb94322397f005adc8b
nasar06 <nasar5847@gmail.com>
2023-02-27 01:01:22 +0600

    create api for update orders

--- routes/orders.route.js
+++ routes/orders.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { newOrder, getallOrderbyEmail, getAllOrders } = require("../controller/orders.controller");
+const { newOrder, getallOrderbyEmail, getAllOrders, updatePayOrder } = require("../controller/orders.controller");
 const router = express.Router();
 
 router.post('/add-order', newOrder)
 router.get('/get-order-byEmail', getallOrderbyEmail)
 router.get('/get-all-orders', getAllOrders)
+router.put('/update-order', updatePayOrder)
 
 module.exports = router;
